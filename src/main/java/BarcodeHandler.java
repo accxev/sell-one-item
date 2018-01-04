d847ba9358df89c91eda57c0a9f8a04b3cdf3150
Antonia Schmalstieg <antonia.schmalstieg@codecentric.de>
2018-01-04 18:49:08 +0100

    Added a second item and a test that checks of the barcode 'is received' internally

--- src/main/java/BarcodeHandler.java
+++ src/main/java/BarcodeHandler.java
@@ -1,11 +1,20 @@
 public class BarcodeHandler {
 
+    private String barcode;
+
 
 
     public void onBarcode(String barcode) {
+        this.barcode = barcode;
     }
 
     public String getLastDisplayedText() {
-        return "$2.50";
+        if (barcode.equals("123")) return "$2.50";
+        if (barcode.equals("111")) return "$1.12";
+        return "";
+    }
+
+    public String getBarcode() {
+        return barcode;
     }
 }
--- src/test/java/sellOneItemTest.java
+++ src/test/java/sellOneItemTest.java
@@ -5,16 +5,27 @@ import static org.junit.Assert.assertEquals;
 public class sellOneItemTest {
 
     @Test
+    public void barcodeGetsReadCorrectlyTest() {
+        BarcodeHandler barcodeHandler = new BarcodeHandler();
+        barcodeHandler.onBarcode("678");
+        assertEquals(barcodeHandler.getBarcode(), "678");
+    }
+
+    @Test
     public void fullDataFlowTest() {
-        String barcode = "123";
 
         BarcodeHandler barcodeHandler = new BarcodeHandler();
+        barcodeHandler.onBarcode("123");
 
-        barcodeHandler.onBarcode(barcode);
+        assertEquals("$2.50", barcodeHandler.getLastDisplayedText());
 
-        String text = barcodeHandler.getLastDisplayedText();
+    }
 
-        assertEquals("$2.50", text);
+    @Test
+    public void fullDataFlowTestSecondItem() {
 
+        BarcodeHandler barcodeHandler = new BarcodeHandler();
+        barcodeHandler.onBarcode("111");
+        assertEquals("$1.12", barcodeHandler.getLastDisplayedText());
     }
 }
